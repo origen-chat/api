d9806e735eea04315c8ad274b7f46de79bf02a25
Diego Stratta <strattadb@gmail.com>
2019-01-21 21:10:25 -0300

    feat: add workspace membership deletion

---
+++ src/core/workspaceMemberships/deletion.ts
@@ -0,0 +1,58 @@
+import db, { maybeAddTransactionToQuery, doInTransaction } from '../db';
+import { DBOptions } from '../types';
+import { workspaceMembershipsTableName } from './constants';
+import { WorkspaceMembership } from './types';
+import { maybeUpdateSubscriptionQuantityInStripe } from './subscriptions';
+import { getWorkspaceById } from '../workspaces';
+import { isUserWorkspaceMembership } from './predicates';
+
+export async function deleteWorkspaceMembership(
+  workspaceMembership: WorkspaceMembership,
+  options: DBOptions = {},
+): Promise<WorkspaceMembership> {
+  await doInTransaction(async transaction => {
+    const optionsWithTransaction = { ...options, transaction };
+
+    await deleteWorkspaceMembershipFromDB(
+      workspaceMembership,
+      optionsWithTransaction,
+    );
+
+    if (isUserWorkspaceMembership(workspaceMembership)) {
+      // eslint-disable-next-line typescript/no-non-null-assertion
+      const workspace = (await getWorkspaceById(
+        workspaceMembership.workspaceId,
+      ))!;
+
+      await maybeUpdateSubscriptionQuantityInStripe(
+        { workspace },
+        optionsWithTransaction,
+      );
+    }
+  }, options);
+
+  return workspaceMembership;
+}
+
+export async function deleteWorkspaceMembershipFromDB(
+  workspaceMembership: WorkspaceMembership,
+  options: DBOptions = {},
+): Promise<WorkspaceMembership> {
+  await doDeleteWorkspaceMembershipFromDB(workspaceMembership, options);
+
+  return workspaceMembership;
+}
+
+export async function doDeleteWorkspaceMembershipFromDB(
+  workspaceMembership: WorkspaceMembership,
+  options: DBOptions = {},
+): Promise<void> {
+  const query = db
+    .delete()
+    .from(workspaceMembershipsTableName)
+    .where({ id: workspaceMembership.id });
+
+  maybeAddTransactionToQuery(query, options);
+
+  await query;
+}
--- src/core/workspaceMemberships/predicates.ts
+++ src/core/workspaceMemberships/predicates.ts
@@ -2,6 +2,11 @@ import { DBOptions } from '../types';
 import { User } from '../users';
 import { Workspace } from '../workspaces';
 import { getWorkspaceMembershipsByWorkspaceAndUsers } from './get';
+import {
+  WorkspaceMembership,
+  UserWorkspaceMembership,
+  BotWorkspaceMembership,
+} from './types';
 
 /**
  * Returns `true` if all the users are members of the workspace.
@@ -19,4 +24,29 @@ export async function areWorkspaceMembers(
   );
 
   return workspaceMemberships.length === users.length;
+}
+
+export function isWorkspaceMembership(
+  value: any,
+): value is WorkspaceMembership {
+  return (
+    typeof value === 'object' &&
+    value &&
+    value.id &&
+    value.workspaceId &&
+    value.role &&
+    (value.botMemberId || value.userMemberId)
+  );
+}
+
+export function isUserWorkspaceMembership(
+  workspaceMembership: WorkspaceMembership,
+): workspaceMembership is UserWorkspaceMembership {
+  return !!workspaceMembership.userMemberId;
+}
+
+export function isBotWorkspaceMembership(
+  workspaceMembership: WorkspaceMembership,
+): workspaceMembership is BotWorkspaceMembership {
+  return !!workspaceMembership.botMemberId;
 }
